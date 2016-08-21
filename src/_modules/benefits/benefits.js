c3a917d6ec1f19fb61336b1294e71534313856f1
migueljrojas <migueljrojas@gmail.com>
2016-08-21 12:19:46 -0300

    Base App structure

---
+++ src/_modules/benefits/benefits.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// Constructor
+var Benefits = function() {
+  this.name = 'benefits';
+  console.log('%s module', this.name);
+};
+
+module.exports = Benefits;
---
+++ src/_modules/gallery/gallery.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// Constructor
+var Gallery = function() {
+  this.name = 'gallery';
+  console.log('%s module', this.name);
+};
+
+module.exports = Gallery;
