2b7101f36d06dab9cf63390baf1ea38c81561b79
migueljrojas <migueljrojas@gmail.com>
2016-08-21 19:06:07 -0300

    Finished Header, Footer and Partners module

--- src/_modules/header/header.js
+++ src/_modules/header/header.js
@@ -1,9 +1,15 @@
 'use strict';
+var $ = require('jquery');
 
 // Constructor
 var Header = function() {
   this.name = 'header';
   console.log('%s module', this.name);
+
+  $('.hamburguer').click(function(){
+    $(this).toggleClass('-open');
+  });
+
 };
 
 module.exports = Header;
